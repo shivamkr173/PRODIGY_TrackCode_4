7d2a3f5181052c998a7562004700b74aed16cfeb
Shivam Kumar <sk9934155716@gmail.com>
2026-04-19 11:27:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,40 +1,35 @@
-const express = require('express');
-const http = require('http');
-const WebSocket = require('ws');
-const cors = require('cors');
-const { addUser, removeUser, getUser } = require('./users');
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
 
 const app = express();
 app.use(cors());
 
 const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
 
-wss.on('connection', (ws) => {
-  ws.on('message', (msg) => {
-    const data = JSON.parse(msg);
-    switch (data.type) {
-      case 'LOGIN':
-        addUser(data.username, ws);
-        break;
-      case 'MESSAGE':
-        const recipient = getUser(data.to);
-        if (recipient) {
-          recipient.send(JSON.stringify({
-            type: 'MESSAGE',
-            from: data.from,
-            content: data.content,
-          }));
-        }
-        break;
-    }
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("join_room", (room) => {
+    socket.join(room);
   });
 
-  ws.on('close', () => {
-    removeUser(ws);
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
   });
 });
 
-server.listen(3001, () => {
-  console.log('WebSocket server running on port 3001');
+server.listen(5000, () => {
+  console.log("Server running on port 5000");
 });
